1d332c05cd3129c362ed29de6bbfd612970167cc
manishfsb <mnshbsnt@gmail.com>
2020-12-08 14:13:31 -0500

    Trying to push multiples and non multiples of 4 characters into the stack and retrieve them

--- project3.s
+++ project3.s
@@ -9,16 +9,20 @@ main:	li $v0, 8
 	syscall
 
 	la $s1, reply
+	li $s3, 1
 	
-	lb $s2, 0($s1)
+Loop1:	lb $s2, 0($s1)
 	addi $sp, $sp, -1
 	sb $s2, 0($sp)
 
 	lb $a0, 0($sp)
 	addi $sp, $sp, 1
 	
-	li $v0, 11
-	syscall
-
+Check:	beq $s3, 4, End
+	
+	addi $s3, $s3, 1
+	addi $s1, $s1, 1
+	j Loop1
+	
 End:	li $v0, 10 
 	syscall	
